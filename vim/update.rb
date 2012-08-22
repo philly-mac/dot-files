03d239273fe7a1f6fa09fb270d0ecbe99037ee19
Philip MacIver <philip@ivercore.com>
2012-08-22 20:50:23 +0200

    Updated vim settings

--- vim/update.rb
+++ vim/update.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'bundles'
 
 if system("which apt-get 2>&1 > /dev/null")
   pkgs = ['git-core', 'vim-gnome', 'ruby', 'ack-grep', 'exuberant-ctags']
@@ -45,33 +46,7 @@ def gitbase
 end
 
 def bundles
-  @bundles ||= [
-    "ack.vim",                                                         # Ack integration
-    "taglist.vim",
-    "fugitive.vim",                                                    # Git integration
-    "Gundo",                                                           # Super undo
-    "rails.vim",                                                       # Rails plugin
-    "repeat.vim",                                                      # Enhanced repeater
-    "scratch-utility",                                                 # Scratch pad
-    ["snipmate.vim","https://github.com/ervandew/snipmate.vim.git"],   # Snippet support
-    ["supertab","http://github.com/ervandew/supertab.git"],            # All completion done via tab key
-    "surround.vim",
-    "The-NERD-Commenter",                                              # Commenting out code
-    "The-NERD-tree",                                                   # File browser
-    "YankRing.vim",                                                    # Copy and paste ring
-    "ZoomWin",                                                         # Zoom in an focus on one window
-    "hexHighlight.vim",
-    "mru.vim",                                                         # List of all the recent files used
-    "Tabular",
-    "haml.zip",                                                        # HAML & SASS syntax file
-    ["delimitMate","https://github.com/Raimondi/delimitMate.git"],     # Auto closing parenthesis
-    "jQuery",                                                          # jQuery Syntax file
-    "JSON.vim",                                                        # JSON Syntax file
-    "matchit.zip",                                                     # Use % to jump to the matching character
-    "VisIncr",                                                         # Making columns of increasing or decreasing numbers, dates, or daynames.
-    "unimpaired.vim",
-    ['ctrlp.vim', 'https://github.com/kien/ctrlp.vim.git' ],
-  ]
+  @bundles
 end
 
 def bundle_names
